487ef60b8d716999e8e451927e8b79e93f0d5364
Ojas Deshpande <ojas.md2006@gmail.com>
2025-06-29 11:35:33 +0530

    backend working on localhost

--- src/store/news-api-store.jsx
+++ src/store/news-api-store.jsx
@@ -6,39 +6,11 @@ export const Headlines = createContext({
   data: [],
   loading: true,
 });
-// {
-//   headers: {
-//     'User-Agent': 'Mozilla/5.0', // some APIs require this
-//   },
-// }
+
 export const HeadlinesProvider = ({ children }) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   let json;
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us`,
-  //       {
-  //         headers: {
-  //           'X-Api-Key': import.meta.env.VITE_NEWS_API_KEY,
-  //           'User-Agent': 'Mozilla/5.0',
-  //           Origin: 'https://quicknewsai.netlify.app/',
-  //         },
-  //       }
-  //     );
-  //     if (!response.ok) throw new Error('Network response was not ok');
-
-  //     json = await response.json();
-  //     setData(json);
-  //   } catch (error) {
-  //     console.error('Fetch error:', error);
-  //   } finally {
-  //     setLoading(false);
-  //     console.log('Headlines Reloaded');
-  //     localStorage.setItem('headlines', JSON.stringify(json));
-  //   }
-  // };
 
   const fetchData = async () => {
     try {
@@ -79,31 +51,6 @@ export const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState('business');
   const [news, setNews] = useState(null);
   const [loading, setLoading] = useState(true);
-  //useEffect(() => {
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${category}`,
-  //       {
-  //         headers: {
-  //           'X-Api-Key': import.meta.env.VITE_NEWS_API_KEY,
-  //           'User-Agent': 'Mozilla/5.0',
-  //           Origin: 'https://quicknewsai.netlify.app/',
-  //         },
-  //       }
-  //     );
-  //     if (!response.ok) throw new Error('Network response was not ok');
-
-  //     const json = await response.json();
-  //     setNews(json);
-  //   } catch (error) {
-  //     console.error('Fetch error:', error);
-  //   } finally {
-  //     setLoading(false);
-  //     console.log('NewsReloaded');
-  //   }
-  //};
-  //fetchData();
 
   useEffect(() => {
     const fetchNews = async () => {
